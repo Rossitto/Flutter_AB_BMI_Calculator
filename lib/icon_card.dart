0c1ebe44af722a62763c5582f0d5a6dc3049d4c4
Rossitto <lucas74rossi@gmail.com>
2020-04-27 15:23:59 -0300

    more dart files for the classes

---
+++ lib/icon_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+TextStyle normalStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+class IconCard extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  IconCard({@required this.icon, @required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: normalStyle,
+        ),
+      ],
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_card.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -35,6 +37,10 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(
                       colour: activeCardColour,
+                      cardChild: IconCard(
+                        icon: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
+                      ),
                     ),
                   ),
                 ],
@@ -68,55 +74,6 @@ class _InputPageState extends State<InputPage> {
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class IconCard extends StatelessWidget {
-  final IconData icon;
-  final String text;
-
-  IconCard({@required this.icon, @required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild});
-
-  final Widget cardChild;
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
