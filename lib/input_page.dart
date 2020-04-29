43723fcb0e7f42cb67cb2d32d932e20c93ca8bbb
Rossitto <lucas74rossi@gmail.com>
2020-04-28 21:09:19 -0300

    ReusableCard receives a function in its constructo

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -30,38 +30,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onPress: () {
                         setState(() {
                           selectedGender = Gender.male;
                         });
                       },
-                      child: ReusableCard(
-                        colour: selectedGender == Gender.male
-                            ? activeCardColour
-                            : inactiveCardColour,
-                        cardChild: IconCard(
-                          icon: FontAwesomeIcons.mars,
-                          text: 'MALE',
-                        ),
+                      colour: selectedGender == Gender.male
+                          ? activeCardColour
+                          : inactiveCardColour,
+                      cardChild: IconCard(
+                        icon: FontAwesomeIcons.mars,
+                        text: 'MALE',
                       ),
                     ),
                   ),
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onPress: () {
                         setState(() {
                           selectedGender = Gender.female;
                         });
                       },
-                      child: ReusableCard(
-                        colour: selectedGender == Gender.female
-                            ? activeCardColour
-                            : inactiveCardColour,
-                        cardChild: IconCard(
-                          icon: FontAwesomeIcons.venus,
-                          text: 'FEMALE',
-                        ),
+                      colour: selectedGender == Gender.female
+                          ? activeCardColour
+                          : inactiveCardColour,
+                      cardChild: IconCard(
+                        icon: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
                       ),
                     ),
                   ),
