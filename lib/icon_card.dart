292ec8fc8b004d15090108f4137b787d825a8fa1
Rossitto <lucas74rossi@gmail.com>
2020-04-28 21:42:54 -0300

    added constants.dart!

--- lib/icon_card.dart
+++ lib/icon_card.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-
-TextStyle normalStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+import 'constants.dart';
 
 class IconCard extends StatelessWidget {
   final IconData icon;
@@ -25,7 +21,7 @@ class IconCard extends StatelessWidget {
         ),
         Text(
           text,
-          style: normalStyle,
+          style: kLabelTextStyle,
         ),
       ],
     );
